45adeb18039bf5560c08b56ba9d94753576954c3
syaring <sarah.k.serim@gmail.com>
2023-08-06 12:21:18 +0900

    add: 사이드바 추가 및 링크 테스트

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,7 +1,34 @@
 import { defineUserConfig } from 'vuepress';
+import { defaultTheme } from '@vuepress/theme-default'
 
 export default defineUserConfig({
   lang: 'en-US',
   title: 'syaring\'s blog',
   description: 'blog service using vuepress',
+  theme: defaultTheme({
+    sidebar: [
+      {
+        text: '테스트 1',
+        link: '/TIL',
+      },
+      {
+        text: '테스트 2',
+        link: '/test2',
+      }
+    ],
+    navbar: [
+      {
+        text: 'Home',
+        link: '/',
+      },
+      {
+        text: 'TIL',
+        link: '/til'
+      },
+      {
+        text: 'GitHub',
+        link: 'https://github.com/syaring/vuepress-blog',
+      }
+    ],
+  }),
 });
---
+++ docs/test2.md
@@ -0,0 +1,6 @@
+---
+lang: en-US
+title: 테스트2
+description: 스터디를 하자! 스터디를 하자!
+---
+테스트 페이지 2
